437b4a6cf2beff69cb280a40a219e07cf895f57e
chao-master <ripppony@gmail.com>
2013-06-17 08:56:41 +0100

    send/recv loops

---
+++ capIzzy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import ircBot
+
+class capIzzy(ircBot.ircBot):
+    pass
+
+capIzzy().connect("irc.canternet.org",6667,"CaptianIzzy")
---
+++ ircBot.py
@@ -0,0 +1,129 @@
+#!/bin/python
+"""
+Create Connection________________
+ |                               |
+Listen for incoming messages    Wait for outgoing messages
+ |
+Process messages and commands
+"""
+
+import Queue
+import socket
+import threading
+
+class command():
+    def __init__(self,minArgs,maxArgs,fun):
+        self.minArgs = minArgs
+        self.maxArgs = maxArgs
+        self.fun = fun
+    
+    def __call__(self,caller,params):
+        try:
+            if len(params) < minArgs:
+                raise Exception     #TODO exception
+            if len(params) > maxArgs:
+                params[minArgs-1:] = [" ".join(params[minArgs-1:])]
+            self.fun(caller,params)
+        except Exception:
+            self.send("PRIVMSG",caller,"Error") #TODO exception
+    
+            
+class ircBot():
+    def __init__(self):
+        self.sendQueue = Queue.Queue()
+        self.socket = socket.socket()
+        self.recvThread = threading.Thread(target=ircBot._recvLoop,args=[self])
+        self.sendThread = threading.Thread(target=ircBot._sendLoop,args=[self])
+        self.commands = {}
+        self._initCommands()
+                    
+    def parseMessage(self,msg):
+        prefix = ['','',''] #Prefix format: Nick/Server,User,Host
+        
+        #Parser assume /r/n has allready been removed
+        if msg[0] == ":": #Check if message contains prefix
+            i = msg.find(" ")
+            if i == -1: raise ParsingError(msg,"Prefix Parsing","No space character after prefix")
+            _prefix = msg[1:i] #Checking for a server name works the same as checking for a nick
+                               #when being lazy, (assuming server name is correctlly formed)
+                               #as it will not contain '!' or '@'
+            msg = msg[i+1:].lstrip(" ")
+            i = _prefix.find("!")
+            j = _prefix.find("@")
+            if i != -1 and j != -1: prefix = [_prefix[:i],_prefix[i+1:j],_prefix[j+1:]]
+            elif i != -1: prefix = [_prefix[:i],_prefix[i+1:],'']
+            elif j != -1: prefix = [_prefix[:j],'',_prefix[j+1:]]
+            else: prefix = [_prefix,'','']
+        i = msg.find(" ")
+        if i == -1: raise ParsingError(msg,"Command Parsing","No space character after command")
+        _command = msg[:i]
+        if _command[0] >= "0" and _command[0] <= "9": #Command is numerical response code (have fun now)
+            if len(_command) != 3: raise ParsingError(msg,"Command Parsing","Numeric code not 3 digits")
+            try: command = int(_command)
+            except ValueError: raise ParsingError(msg,"Command Parsing","Numeric code is not numeric")
+        else: #Command is alpha
+            for l in _command:
+                if (l<"A")|(("Z"<l) & (l<"a"))|("z"<l): raise ParsingError(msg,"Command Parsing","Alpha Command '{0}' contains non-alphas '{1}'".format(_command,l))
+            command = _command.upper()
+        msg = msg[i+1:].lstrip(" ")
+        params = []
+        while len(msg) != 0:
+            if msg[0] == ":": #Paramater is a trailing
+                params.append(msg[1:])
+                break
+            else: #Paramater is non-trailing
+                i = msg.find(" ")
+                if i == -1: #Last paramater
+                    params.append(msg)
+                    break
+                else:
+                    params.append(msg[:i])
+                    msg = msg[i+1:]
+        return prefix,command,params
+    
+    def _sendLoop(self):
+        while 1:
+            message = self.sendQueue.get(True)
+            print "^^", message
+            self.socket.sendall(message)
+            self.sendQueue.task_done()
+    
+    def _recvLoop(self):
+        while 1:
+            buff= ""
+            while (buff[-2:] != "\r\n"):
+                buff = buff + self.socket.recv(1024)
+            for message in buff.split("\r\n"):
+                if message == "": continue
+                prefix,command,params = self.parseMessage(message)
+                if command == "PING":
+                    self.send("PONG",params)
+                elif command == "PRIVMSG":
+                    print params[0],"|",prefix[0],">",params[1]
+                    if params[1][0] == ".": #TODO magic character
+                        botCmd,botParams = params[1][1:].split(" ",2)
+                        self.commands[botCommand](prefix[1],botParams)
+                else:
+                    print prefix,command,params
+                #TODO handle nick changes ect."""
+            
+    
+    def send(self,command,*params):
+        if len(params) > 0:
+            message = command+" "+" ".join(params[:-1])+" :"+params[-1]
+        else:
+            message = command
+        self.sendQueue.put(message)
+    
+    def connect(self,host,port,*nicks):
+        self.socket.connect((host,port))
+        self.recvThread.start()
+        self.sendThread.start()
+        self.send("NICK",nicks[0])
+        self.send("USER","pythonIrcBot",".",".")
+    
+    
+    ### Commands ###
+    
+    def _initCommands(self):
+        pass
